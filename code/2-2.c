deaed077ea0209833c9e8467aec8a2e6347d0b3e
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-04-24 11:41:07 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+VOID foo() {
+    char *str1 = "hello";
+    printf("%p\n", (void*)str1)
+}
+
+void bar() {
+    char *str2 = "hello";
+    printf("%p\n", (void*)str2)
+}
+
+int main() {
+
+    foo();
+    bar();
+  
+  return 0;
+}
