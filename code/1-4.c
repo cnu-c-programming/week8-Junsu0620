6876afbae50f06b430cb0ba5ed713cead4cfb417
Junsu0620 <150302819+Junsu0620@users.noreply.github.com>
2026-04-30 16:56:53 +0900

    Update 1-4.c

---
+++ code/1-4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int my_strcmp(const char *a, const char *b) {
+    while {
+        while (*a == ' ') {
+            a++;
+        }
+        while (*b == ' ') {
+            b++;
+        }
+        if (*a != *b) {
+            return *a - *b;
+        }
+        if (*a == '\0') {
+            return 0;
+        }
+        a++;
+        b++;
+    }
+}
+
+int main() {
+
+  printf("%d\n", my_strcmp("abc", "abcd"));
+  printf("%d\n", my_strcmp("abc", "abc "));
+  printf("%d\n", my_strcmp("b  ", "a   "));
+
+  return 0; 
+}
